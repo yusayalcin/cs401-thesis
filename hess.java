b49419f08ec17a74bb5010895cb81ec187310088
yusayalcin <52274526+yusayalcin@users.noreply.github.com>
2019-12-01 14:04:03 +0300

    Update hess.java

--- hess.java
+++ hess.java
@@ -1,23 +1,106 @@
 package buffreader;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class hess {
+	static ArrayList<String> arr1 = new ArrayList<String>();
+	 static ArrayList<String> arr2 = new ArrayList<String>();
+
+	   public void printAllSubSequences(String[] arrInput){
+	        int [] temp = new int[arrInput.length];
+	        int index = 0;
+	        solve(arrInput, index, temp);
+	    }
+	   
+
+	    private void solve(String[] arrInput, int index, int [] temp){
+	        if(index==arrInput.length){
+	            print(arrInput,temp);
+	            return;
+	        }
+	        //set the current index bit and solve it recursively
+	        temp[index] = 1;
+	        solve(arrInput,index+1,temp);
+	        //unset the current index bit and solve it recursively
+	        temp[index] = 0;
+	        solve(arrInput,index+1,temp);
+	    }
+
+	    private void print(String[] arrInput, int [] temp){
+	        String result = "";
+	        for (int i = 0; i <temp.length ; i++) {
+	            if(temp[i]==1)
+	                result += arrInput[i]+" ";
+	        }
+	      //  if(result=="")
+	           // result = "{Empty Set}";
+	        arr1.add(result);
+	        //System.out.println(result);
+	    }
+	    public void printAllSubSequences1(String[] arrInput){
+	        int [] temp = new int[arrInput.length];
+	        int index = 0;
+	        solve1(arrInput, index, temp);
+	    }
+	   
+
+	    private void solve1(String[] arrInput, int index, int [] temp){
+	        if(index==arrInput.length){
+	            print1(arrInput,temp);
+	            return;
+	        }
+	        //set the current index bit and solve it recursively
+	        temp[index] = 1;
+	        solve1(arrInput,index+1,temp);
+	        //unset the current index bit and solve it recursively
+	        temp[index] = 0;
+	        solve1(arrInput,index+1,temp);
+	    }
+
+	    private void print1(String[] arrInput, int [] temp){
+	        String result = "";
+	        for (int i = 0; i <temp.length ; i++) {
+	            if(temp[i]==1)
+	                result += arrInput[i]+" ";
+	        }
+	        //if(result=="")
+	           // result = "{Empty Set}";
+	        arr2.add(result);
+	        //System.out.println(result);
+	    }
+
+	    public static void main(String[] args) {
+	        String [] s1 = {"b","a","b","a","b"};
+	        String [] s2 = {"b","a","b","b","a"};
+
+	       
+
+	        new hess().printAllSubSequences(s1);
+	        new hess().printAllSubSequences1(s2);
+
+	        System.out.println(arr1);
+	        System.out.println(arr2);
+	        arr1.removeAll(arr2);
+	        System.out.println(arr1);  
+
+	    }
 	// set to store all the subsequences 
-	   public static HashSet<String> st = new HashSet<>(); 
+	  /* public static HashSet<String> st = new HashSet<>(); 
 	   public static HashSet<String> st1 = new HashSet<>(); 
+       //static ArrayList<String> sa = new ArrayList<String>();
 
 	  
 	    // It computes all the subsequence of an string 
-	   public static void subsequence(String str) 
+	   public static void subsequence(String string) 
 	    { 
 	        // iterate over the entire string 
-	        for (int i = 0; i < str.length(); i++) { 
+	        for (int i = 0; i < string.length(); i++) { 
 	              
 	            // iterate from the end of the string 
 	            // to generate substrings 
-	            for (int j = str.length(); j > i; j--) { 
-	                String sub_str = str.substring(i, j); 
+	            for (int j = string.length(); j > i; j--) { 
+	                String sub_str = string.substring(i, j); 
 	              
 	                if (!st.contains(sub_str)) 
 	                    st.add(sub_str); 
@@ -66,9 +149,9 @@ public class hess {
 	      // Driver code 
 	    public static void main(String[] args) 
 	    { 
-	        String s = "babab"; 
-	        String s1 = "babba"; 
 
+	    	String s = "babab"; 
+	        String s1 = "babba"; 
 	        subsequence(s); 
 	        subs(s1); 
 
@@ -81,5 +164,5 @@ public class hess {
 	        st.removeAll(st1);
 	        System.out.println(st);  //ilkinin ikincisine göre farklarını basıyor
 
-	    } 
+	    } */
 }
