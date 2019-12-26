b48a8ebeda45e0114b2afcccd580d71030594589
ahmetgokce <43876641+ahmetgokce@users.noreply.github.com>
2019-12-26 18:48:47 +0300

    Update hess.java

--- hess.java
+++ hess.java
@@ -1,14 +1,17 @@
-
 package buffreader;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-public class hess {
+
+
+  class hess {
 	static ArrayList<String> arr1 = new ArrayList<String>();
 	static ArrayList<String> arr2 = new ArrayList<String>();
 
@@ -75,8 +78,7 @@ public class hess {
 	}
 
 	public static void main(String[] args) throws IOException {
-		String[] s1 = read("C:\\Users\\ahmet\\Desktop\\bitirme\\producerconsumer\\1575371279064Out.txt");
-		String[] s2 = read("C:\\Users\\ahmet\\Desktop\\bitirme\\producerconsumer\\1575371318152Out.txt");
+		read("C:\\Users\\ahmet\\Desktop\\bitirme\\producerconsumer\\1577307556703Out.txt","C:\\Users\\ahmet\\Desktop\\bitirme\\producerconsumer\\bug.txt");
 
 		/*new hess().printAllSubSequences(s1);
 		new hess().printAllSubSequences1(s2);
@@ -88,10 +90,43 @@ public class hess {
 
 	}
 
-	private static String[] read(String location) throws IOException {
-		BufferedReader reader1 = new BufferedReader(new FileReader(location));
-		String line;
-		ArrayList<String> lines = new ArrayList<String>();
+	private static void read(String success,String fail) throws IOException {
+		PrintStream text = new PrintStream(new File("C:\\Users\\ahmet\\Desktop\\bitirme\\buffreader\\difference\\"+System.currentTimeMillis()+"Differences.txt"));
+		System.setOut(text);
+		BufferedReader reader1 = new BufferedReader(new FileReader(success));
+		BufferedReader reader2 = new BufferedReader(new FileReader(fail));
+		String line1 ;
+        String line2 ;
+        ArrayList<String> result = new ArrayList<String>();
+        ArrayList<String> text1 = new ArrayList<String>();
+		ArrayList<String> text2 = new ArrayList<String>();
+		while ((line1 = reader1.readLine()) != null) {
+			text1.add(line1);
+		}
+		while ((line2 = reader2.readLine()) != null) {
+			text2.add(line2);
+		}
+		if(text2.size() > text1.size()) {
+			for(int i = 0; i<text1.size(); i++) {
+				if(!text1.get(i).equals(text2.get(i))) {
+					result.add(text2.get(i));
+				}
+			}
+			for(int j = text1.size(); j < text2.size(); j++) {
+				result.add(text2.get(j));
+			}
+			
+		}
+		else {
+			for(int i = 0; i<text2.size(); i++) {
+				if(!text1.get(i).equals(text2.get(i))) {
+					result.add(text2.get(i));
+				}
+			}	
+		}
+		
+        	
+		/*ArrayList<String> lines = new ArrayList<String>();
 		while ((line = reader1.readLine()) != null) {
 			lines.add(line);
 		}
@@ -103,5 +138,11 @@ public class hess {
 			System.out.println(result[i]);
 		}
 		return result;
-	}
+	}*/	
+        for(int i = 0; i < result.size(); i++) {
+        	System.out.println(result.get(i));
+        
+        }
+    }
 }
+
