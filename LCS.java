6fe6dd019d56d89ce7fdd346c123c38be27b4e59
ahmetgokce <43876641+ahmetgokce@users.noreply.github.com>
2020-04-06 21:42:36 +0300

    Update LCS.java

--- LCS.java
+++ LCS.java
@@ -1,4 +1,9 @@
+package buffreader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,19 +12,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class LCS
-{
+public class LCS {
     // Function to find LCS of String X[0..m-1] and Y[0..n-1]
-    public static String lcs(String[] x, String[] y, int m, int n, int[][] T)
-    {
+    public static String lcs(String[] x, String[] y, int m, int n, int[][] T) {
         // return empty string if we have reached the end of
         // either sequence
         if (m == 0 || n == 0) {
             return "";
         }
         // if last character of X and Y matches
-        if (Objects.equals(x[m - 1], y[n - 1]))
-        {
+        if (Objects.equals(x[m - 1], y[n - 1])) {
             // append current character (X[m-1] or Y[n-1]) to LCS of
             // substring X[0..m-2] and Y[0..n-2]
             return lcs(x, y, m - 1, n - 1, T) + x[m - 1];
@@ -33,8 +35,7 @@ public class LCS
 
         if (T[m - 1][n] > T[m][n - 1]) {
             return lcs(x, y, m - 1, n, T);
-        }
-        else {
+        } else {
             // if left cell of current cell has more value than the top
             // cell, then drop current character of String Y and find LCS
             // of substring X[0..m-1], Y[0..n-2]
@@ -45,13 +46,10 @@ public class LCS
 
     // Function to fill lookup table by finding the length of LCS
     // of substring X[0..m-1] and Y[0..n-1]
-    public static void lcsLength(String[] x, String[] y, int m, int n, int[][] T)
-    {
+    public static void lcsLength(String[] x, String[] y, int m, int n, int[][] T) {
         // fill the lookup table in bottom-up manner
-        for (int i = 1; i <= m; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
 
                 // if current character of X and Y matches
                 if (Objects.equals(x[i - 1], y[j - 1])) {
@@ -67,39 +65,40 @@ public class LCS
     }
 
     // main function
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
 
-        String[] X =  read("location");
-        String[] Y = read("bug location");
-        System.out.println(Arrays.toString(X));
+        String[] successful = read("C:\\Users\\ahmet\\Desktop\\401-demo\\success.txt");
+        String[] failed = read("C:\\Users\\ahmet\\Desktop\\401-demo\\bug.txt");
+        System.out.println(Arrays.toString(successful));
         //String[] X = {"X","M","J","Y","A","U","Z"}, Y = {"M","Z","J","A","W","X","U"};
-        int m = X.length, n = Y.length;
+        int m = successful.length, n = failed.length;
 
 
         // T[i][j] stores the length of LCS of substring
         // X[0..i-1], Y[0..j-1]
         int[][] T = new int[m + 1][n + 1];
         // fill lookup table
-        lcsLength(X, Y, m, n, T);
+        lcsLength(successful, failed, m, n, T);
 
-        String[] arr = lcs(X, Y, m, n, T).split("!");
+        String[] arr = lcs(successful, failed, m, n, T).split("!");
         ArrayList<String> longest = new ArrayList<String>();
         ArrayList<String> fail = new ArrayList<String>();
         for (String s : arr)
-            longest.add(s+"!");
-        for (String  s : longest)
+            longest.add(s + "!");
+        for (String s : longest)
             System.out.println(s);
-        for(String s : Y)
+        for (String s : failed)
             fail.add(s);
         //for (String  s : fail)
-            //System.out.println(s);
+        //System.out.println(s);
         System.out.println("----------");
-       ArrayList<String> result =  shortestUnCommonPath(longest,fail,0,0);
+        PrintStream text = new PrintStream(new File("C:\\Users\\ahmet\\Desktop\\401-demo\\"+System.currentTimeMillis()+"Differences.txt"));
+		System.setOut(text);
+        ArrayList<String> result = shortestUnCommonPath(longest, fail, 0, 0);
 
-       for(String s : result) System.out.println(s);
+        for (String s : result) System.out.println(s);
         // find longest common sequence
-       // System.out.print(Arrays.toString(arr));
+        // System.out.print(Arrays.toString(arr));
         System.exit(0);
     }
 
@@ -108,35 +107,34 @@ public class LCS
         ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(path);
         return lines.toArray(new String[0]);
     }
-    public static ArrayList<String> shortestUnCommonPath(ArrayList<String> longest, ArrayList<String> fail, int indexLongest, int indexFail){
-        int nextFailIn = 0;
-        int indexCommon= indexLongest;
-        if(indexCommon == longest.size()){
+    public static ArrayList<String> shortestUnCommonPath(ArrayList<String> longest, ArrayList<String> fail, int indexLongest, int indexFail) throws FileNotFoundException {
+    	int nextFailIn = 0;
+        int indexCommon = indexLongest;
+        if (indexCommon == longest.size()) {
             return fail;
         }
-        if (indexFail  == fail.size()) {
+        if (indexFail == fail.size()) {
             return fail;
-        }
-         else if (indexFail +1 == fail.size()){
+        } else if (indexFail + 1 == fail.size()) {
             return fail;
-        }
-        else {
+        } else {
             for (int i = indexFail; i < fail.size(); i++) {
-                   // if (fail.get(i).equals(longest.get(indexLongest))) {
-                     //   System.out.println("****");
+                if (fail.get(i).equals(longest.get(indexLongest))) {
+                    //   System.out.println("****");
                     fail.remove(i);
                     //for (String  s : fail)
-                      //  System.out.println(s);
-                    if(i == 0)
+                    //  System.out.println(s);
+                    if (i == 0)
                         nextFailIn = i;
                     else
-                        nextFailIn = i-1;
+                        nextFailIn = i - 1;
                     indexCommon++;
                     break;
                 }
             }
             shortestUnCommonPath(longest, fail, indexCommon, nextFailIn);
 
-        return fail;
+            return fail;
+        }
     }
 }
